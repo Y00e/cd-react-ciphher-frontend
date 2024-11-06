2535f78ce593d00362714a848787b67a89b814ab
Yoe <dembouz719@gmail.com>
2024-11-06 15:35:40 +0100

    Moved App jsx in to components map

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import '../styles/App.css'
+import Cipher from './Cipher';
+
+
+function App() {
+
+  return (
+      <>
+          <div>
+              <h1>Cipher App</h1>
+              <Cipher/>
+          </div>
+      </>
+  )
+}
+
+export default App
